d3083a3222c59e9d819287804ca46cb9e2bd68cb
Zheming Li <nkdudu@126.com>
2022-09-29 18:37:50 +0800

    nvim add dirs

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -21,6 +21,23 @@ vim.opt.softtabstop     = 2
 vim.opt.expandtab       = true
 vim.cmd('autocmd Filetype go setlocal ts=4 sw=4 sts=4 expandtab')
 
+-- better backup, swap and undos storage
+vim.cmd('set backup')
+vim.cmd('set undofile')
+local dirs = {
+  vim.fn.expand('~/.vim/dirs/tmp'),  -- place swap files
+  vim.fn.expand('~/.vim/dirs/backups'), -- place backup files
+  vim.fn.expand('~/.vim/dirs/undos'), -- place undos
+}
+vim.opt.directory = dirs[0]
+vim.opt.backupdir = dirs[1]
+vim.opt.undodir = dirs[2]
+for i, dir in ipairs(dirs) do
+  if vim.fn.isdirectory(dir) == 0 then
+    vim.fn.mkdir(dir, "p")
+  end
+end
+
 require("core.plugins")
 require("core.keymaps")
 require("impatient")
